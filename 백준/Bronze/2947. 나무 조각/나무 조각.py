a5f79c57f34aca5f3869d46a9f3ff51191acf3fd
Rina Jung <82943421+sungshin52@users.noreply.github.com>
2023-01-18 16:18:06 +0900

    [Bronze I] Title: 나무 조각, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,11 @@
+wood = list(map(int, input().split()))
+wood_sorted = [1, 2, 3, 4, 5]
+
+while True:
+    for i in range (len(wood)-1):
+        if (wood[i] > wood[i+1]):
+            wood[i], wood[i+1] = wood[i+1], wood[i]
+            print(*wood)
+    
+    if wood == wood_sorted:
+        break
